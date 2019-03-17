ebae1aba584b4adb9585a447a84e7ee75a603372
Anisul Kibria <du.shaheen@gmail.com>
2019-03-17 21:22:07 +0600

    footer and form style edited

--- footer.php
+++ footer.php
@@ -17,6 +17,7 @@
 	<div id="footer" class="container-fluid">	
 	<footer id="colophon" class="site-footer">
 		<div class="site-info text-center">
+			<img class="ehllogo" src="<?php echo get_template_directory_uri() . '/img/equal-housing-lender-logo.png'?>" alt=""><br> <p><b>Company NMLS 1653111</b></p>
 			Copyright © 2019 Fast Fund Mortgage<br>
 			Web Site Designed by <a href="http://fastfundmortgage.com/">Fast Fund Mortgage</a><br><br>
 			<p>Disclaimer: please make sure to call our office before sending any sensitive financial information. <br>Online frauds are high and we want to make sure your information is safe and secure.</p>
