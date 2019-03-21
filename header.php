23bcba8c87c8585e79bd1bc0110758036540ff92
Anisul Kibria <du.shaheen@gmail.com>
2019-03-21 12:56:46 +0600

    Combined Header

--- header.php
+++ header.php
@@ -21,38 +21,67 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+<div id="page" class="site homepage">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'fastfundmortgage' ); ?></a>
-<div class="container">
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$fastfundmortgage_description = get_bloginfo( 'description', 'display' );
-			if ( $fastfundmortgage_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $fastfundmortgage_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+<div class="container main-container">
+	<div class="row">
 
+			<div class="col-md-6">
+				<div class="logo">
+					<?php
+					the_custom_logo();
+					if ( is_front_page() && is_home() ) :
+						?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php
+					else :
+						?>
+
+					<?php endif; ?>
+				</div>
+
+			</div>
+
+			<div class="col-md-6">
+				<div class="top-menu"> 
+					<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+						<div class="container">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon"></span>
+							</button>
+								<?php
+								wp_nav_menu( array(
+									'theme_location'    => 'menu-2',
+									'depth'             => 2,
+									'container'         => 'div',
+									'container_class'   => 'collapse navbar-collapse',
+									'container_id'      => 'bs-example-navbar-collapse-1',
+									'menu_class'        => 'nav navbar-nav',
+									'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+									'walker'            => new WP_Bootstrap_Navwalker(),
+								) );
+								?>
+						</div>
+					</nav>
+						<h4 class="contact"><a href="tel:714-692-2955"><b><i class="fas fa-phone"></i>714-692-2955</b></a>
+						<a href="tel:909-395-6474"><b><i class="fas fa-phone"></i>909-395-6474</b></a></h4>
+				</div>
+			</div>
+
+	</div>
+	
+	<div class="row main-menu">
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'fastfundmortgage' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'fastfundmortgage' ); ?></button>
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+				?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	</div>
+
 
 	<div id="content" class="site-content">
